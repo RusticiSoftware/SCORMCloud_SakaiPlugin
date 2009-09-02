77fde528806ac4d09d886e86aaade1db65f62bcd
David Ells <david.ells@scorm.com>
2009-09-02 18:40:30 +0000

    Move some doc files around
    git-svn-id: https://svn.rusticisoftware.com/svn/projects/Products/IKI/CloudIntegrations/sakai@9789 ad08c97e-9a45-6745-9bb2-ebd1718b8280

--- install_files/create-binary-dist.sh
+++ install_files/create-binary-dist.sh
@@ -7,8 +7,8 @@ mkdir ${OUTPUT_FOLDER}
 cp -rv ${CATALINA_HOME}/components/scormcloud-pack ${OUTPUT_FOLDER}/
 cp -v  ${CATALINA_HOME}/shared/lib/scormcloud-api-${VERSION}.jar ${OUTPUT_FOLDER}/
 cp -v  ${CATALINA_HOME}/webapps/scormcloud-tool.war ${OUTPUT_FOLDER}/
-cp binary-install.txt ${OUTPUT_FOLDER}/README.txt
-cp adding_cloud_tool_icon.txt ${OUTPUT_FOLDER}/
+cp -rv ../doc/ ${OUTPUT_FOLDER}/
+cp -v ../doc/binary-install.txt ${OUTPUT_FOLDER}/INSTALL.txt
 cp install.sh ${OUTPUT_FOLDER}/
 cp install.bat ${OUTPUT_FOLDER}/
 zip -r scormcloud-sakai-plugin-${VERSION}.zip ${OUTPUT_FOLDER}/
