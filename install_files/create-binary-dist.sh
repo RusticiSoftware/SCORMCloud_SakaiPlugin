72497d09183643e2dc45582bdcc7bc834d9ac68f
David Ells <david.ells@scorm.com>
2009-11-10 19:22:27 +0000

    No need to look for specific version when copying scorm cloud api jar
    git-svn-id: https://svn.rusticisoftware.com/svn/projects/Products/IKI/CloudIntegrations/sakai/branches/2-5-x@10407 ad08c97e-9a45-6745-9bb2-ebd1718b8280

--- install_files/create-binary-dist.sh
+++ install_files/create-binary-dist.sh
@@ -6,7 +6,7 @@ OUTPUT_FOLDER=${OUTPUT_NAME}
 
 mkdir ${OUTPUT_FOLDER}
 cp -rv ${CATALINA_HOME}/components/scormcloud-pack ${OUTPUT_FOLDER}/
-cp -v  ${CATALINA_HOME}/shared/lib/scormcloud-api-${VERSION}.jar ${OUTPUT_FOLDER}/
+cp -v  ${CATALINA_HOME}/shared/lib/scormcloud-api-*.jar ${OUTPUT_FOLDER}/
 cp -v  ${CATALINA_HOME}/webapps/scormcloud-tool.war ${OUTPUT_FOLDER}/
 cp -rv ../doc/ ${OUTPUT_FOLDER}/
 cp -v ../doc/binary-install.txt ${OUTPUT_FOLDER}/INSTALL.txt
