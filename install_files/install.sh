fd0295bedc631264f3cc3c5cd526b91ce6d15f3f
David Ells <david.ells@scorm.com>
2009-11-10 19:21:59 +0000

    No need to look for specific version when copying scorm cloud api jar
    git-svn-id: https://svn.rusticisoftware.com/svn/projects/Products/IKI/CloudIntegrations/sakai/branches/2-6-x@10406 ad08c97e-9a45-6745-9bb2-ebd1718b8280

--- install_files/install.sh
+++ install_files/install.sh
@@ -2,6 +2,6 @@
 
 echo "Copying SCORM Cloud plugin files to ${CATALINA_HOME}..."
 cp -rv scormcloud-pack ${CATALINA_HOME}/components/
-cp -v scormcloud-api-0.8.jar ${CATALINA_HOME}/shared/lib/
+cp -v scormcloud-api-*.jar ${CATALINA_HOME}/shared/lib/
 cp -v scormcloud-tool.war ${CATALINA_HOME}/webapps/
 echo "Copying complete."
